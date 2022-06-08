6851061e49770486ce791d2db558f0ac0adcb69c
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-08 11:35:58 -0300

    Work in TDD pt 1

---
+++ src/main/java/br/ce/waquino/exceptions/NaoPodeDividirPorZeroException.java
@@ -0,0 +1,7 @@
+package br.ce.waquino.exceptions;
+
+public class NaoPodeDividirPorZeroException extends Exception {
+
+	private static final long serialVersionUID = -2037241482510017010L;
+
+}
---
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -0,0 +1,26 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.waquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class Calculadora {
+
+	public int somar(int a, int b) {
+		return a + b;
+	}
+
+	public int subtrair(int a, int b) {
+		return a - b;
+	}
+
+	public int dividir(int a, int b) throws NaoPodeDividirPorZeroException {
+		if(b == 0) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		return a / b;
+	}
+
+	public int multiplicacao(int a, int b) {
+		return a * b;
+	}
+
+}
